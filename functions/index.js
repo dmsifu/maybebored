2c62f9e77cd1cb53522db2cacd98d28ec08da51b
00LX <laxus_z@hotmail.com>
2022-03-07 14:24:55 -0600

    reworked topics functionality and made it into a fun game

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const axios  = require('axios')
+require('dotenv').config()
+const APIKEY = process.env.APIKEY
+
+exports.handler = async function(event){
+    const activityName = event.queryStringParameters.search
+
+    const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?key=${APIKEY}&part=snippet&maxResults=15&order=viewCount&q=${activityName}`) 
+
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data.items)
+        }
+    } catch (error) {
+        return {
+            statusCode: 422,
+            body: `Error: ${error.stack}`
+        }            
+    }
+}
