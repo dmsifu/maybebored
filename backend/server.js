8fe63b894bf6174445d3ccbdd35c2ace7dde9b14
dmsifu <dmsifu@gmail.com>
2022-06-17 14:39:34 -0500

    preparing for deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,22 @@ connectDB()
 
 const app = express()
 
-addToDatabase()
+// addToDatabase()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api', require('./routes/routes'))
+
+//serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
+else{
+    app.get('/', (req,res) => res.send('Please set to production'))
+}
 
 app.listen(port, ()=>{
     console.log(`server started on ${port}`)
